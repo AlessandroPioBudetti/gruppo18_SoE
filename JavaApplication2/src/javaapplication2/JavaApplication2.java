94bccef60d62fef8bee2b1c0bb8d69fe979a3448
Alessandro Pio Budetti <a.budetti2@studenti.unisa.it>
2020-11-21 12:57:00 +0100

    Revert "Merge branch 'main' of https://github.com/AlessandroPioBudetti/gruppo18_SOE into main"
    This reverts commit dc0c1cce1f8d5e6c1baa14cc6830fa33bb5ab874, reversing
    changes made to c5dbcb599597392105d9390472718c28075f6ec8.

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,9 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        System.out.println("Hello dkwdhwe");
-
+        System.out.println("Hello World1");
     }
     
 }
