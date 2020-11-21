5a3d329df9dc72d9db9204d6bc739d1699f3160b
SabrinaAgnes <74819052+SabrinaAgnes@users.noreply.github.com>
2020-11-21 17:30:53 +0100

    modifica alla stampa

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World1");
+        System.out.println("Hello Team!!!!!!!");
     }
     
 }
