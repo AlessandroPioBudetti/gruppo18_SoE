0754aa68f0ddd79feb7bd29638742bfc35048b21
Marina9-Carratu <74829494+Marina9-Carratu@users.noreply.github.com>
2020-11-21 17:56:31 +0100

    modifica yo
    ciao raga

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -19,6 +19,8 @@ public class JavaApplication2 {
         System.out.println("Ciao ragazzi");
         System.out.println("Ciao ragazzi???");   
         int g=9;
+        System.out.println("yo");
+        int yo=1;
     }
     
 }
