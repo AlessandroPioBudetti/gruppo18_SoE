0fe8941a33a4a03d204f27412e484ee6c5207927
Marina9-Carratu <74829494+Marina9-Carratu@users.noreply.github.com>
2020-12-05 23:42:33 +0100

    creazione classe GuiSiteManagment e TableSite
    Da terminare

--- gruppo18/src/tablepackage/TableSite.java
+++ gruppo18/src/tablepackage/TableSite.java
@@ -1,33 +1,32 @@
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Random;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package tablepackage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
  * @author 39392
  */
-public class TableType {
-  
+public class TableSite {
+   
     private Statement stm;
 
-    public TableType(Statement stm) {
+    public TableSite(Statement stm) {
         this.stm=stm;
     }
-     
-    public boolean insert(String type){
+
+    public boolean insert(String site){
         StringBuilder temp= new StringBuilder();
-        temp.append("insert into Typology(tipo)");
+        temp.append("insert into Site(name_site)");
         temp.append("values(");
-        temp.append("'").append(type).append("'");
+        temp.append("'").append(site).append("'");
         temp.append(")");
         try{
         stm.executeUpdate(temp.toString());
@@ -38,12 +37,12 @@ public class TableType {
         }
     }
   
-    public boolean update(String type, String typeUpdate){
+    public boolean update(String site, String siteUpdate){
         StringBuilder temp= new StringBuilder();
-        temp.append("update Typology set tipo=");
-        temp.append("'").append(typeUpdate).append("'");
-        temp.append(" where tipo=");
-        temp.append("'").append(type).append("'");
+        temp.append("update Site set name_site=");
+        temp.append("'").append(siteUpdate).append("'");
+        temp.append(" where name_site=");
+        temp.append("'").append(site).append("'");
         try{
         stm.executeUpdate(temp.toString());
         return true;
@@ -53,11 +52,11 @@ public class TableType {
         }
     }
     
-    public boolean delete(String type){
+    public boolean delete(String site){
         StringBuilder temp= new StringBuilder();
-        temp.append("delete from Typology ");
-        temp.append("where tipo = ");
-        temp.append("'").append(type).append("'");
+        temp.append("delete from Site ");
+        temp.append("where name_site = ");
+        temp.append("'").append(site).append("'");
         try{
         stm.executeUpdate(temp.toString());
         return true;
@@ -69,19 +68,18 @@ public class TableType {
     
     public ArrayList<String> visualize(){
     StringBuilder temp= new StringBuilder();
-    ArrayList <String> types=new ArrayList();
-    String type;
-        temp.append("select * from Typology");    
+    ArrayList <String> sites=new ArrayList();
+    String site;
+        temp.append("select * from Site");    
         try{
          ResultSet rst=stm.executeQuery(temp.toString());
          while(rst.next()){
-             type=rst.getString("tipo");
-             types.add(type);
+             site=rst.getString("name_site");
+             sites.add(site);
          }     
         }catch(SQLException ex){
             System.out.println(ex.getMessage());
         }
-     return types; 
+     return sites; 
     }
-   
 }
